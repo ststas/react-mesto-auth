fde4ac004acb74b65a7e9a7ebe3e52e98fc187d8
Stas Zaitsev <ststas@yandex.ru>
2023-07-19 18:11:01 +0300

    update basename

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import App from './components/App'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/projects/mestoreact">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
