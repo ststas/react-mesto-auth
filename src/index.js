4019d02689c3e99748034d7799422cb4e660a450
Stas Zaitsev <ststas@yandex.ru>
2023-07-19 18:25:41 +0300

    add RouterProvider & createHashRouter

--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import './pages/index.css'
 import App from './components/App'
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 
+const router = createHashRouter([
+  {
+    path: '/*',
+    element: <App />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/projects/mestoreact">
+    <RouterProvider router={router}>
       <App />
-    </BrowserRouter>
+    </RouterProvider>
   </React.StrictMode>,
 )
